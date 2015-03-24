1fae60571c6e40cf0ec3c5d9878228d74f7c56e0
Omar Gammoh <omar.gammoh@gmail.com>
2015-03-24 16:23:03 +0100

    setup update

--- README.md
+++ README.md
@@ -6,4 +6,11 @@
 There are many things a raspberry pi can do (e.g. stream video, record data, datasend_app data, ...)
 This is a way to setup a server on raspberry pi using django, where every different thing a raspberry pi can do is coded in an django app. The raspberry pi and the apps are controlled with the django server
 
+### how to easily install ###
+* `sudo apt-get install git`
+* `sudo git clone -b master https://omargammoh@github.com/omargammoh/spislave.git`
+* edit `sudo nano ~/rpislave/conf.json`
+* `sudo python ~/rpislave/setup.py`
+* `sudo reboot`
+
 # Currently under construction ... #
--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
-#sudo apt-get install git
-#sudo git clone -b master https://omargammoh@github.com/omargammoh/pylog485.git
-#sudo python ~/pylog485/setup.py
-
 import subprocess
 import os
 import json
 
 try:
-    jsonfile = os.path.join(__file__,'setup.json')
-    #jsonfile = r"C:\Users\omarg_000\Google Drive\Code\pylog485\setup.json"
+    jsonfile = os.path.join(__file__,'conf.json')
 
     if not os.path.isfile(jsonfile):
         raise BaseException('the json config file was not found %s' %jsonfile)
@@ -19,11 +14,6 @@ try:
 except:
     print 'error while getting the json configuration file'
     raise
-
-"""
-"{{{0[network][address]}dfs df s df s}}"
-""".format(js)
-
 
 def execute(lis):
     if type(lis) == str:
@@ -36,64 +26,50 @@ def execute(lis):
     return out
 
 def setup_autologin():
-    #TODO: put the right variables here!
+    # replaces a line in a file
+
     filepath=r"/etc/inittab"
-    linestarttocomment = "1:2345:respawn:/sbin/getty "
+    linetocomment = "1:2345:respawn:/sbin/getty "
     linetoappend = "1:2345:respawn:/bin/login -f pi tty1 </dev/tty1 >/dev/tty1 2>&1"
 
     f = file(filepath, "r")
-    lines = f.readlines()
+    text = f.read()
     f.close()
-    newlines = []
-    done = False
-    alreadydone = False
 
-    for line in lines:
-        if line.startswith(linestarttocomment):
-            newlines.append("#the following line is commented by the script, the line after is appended by the script\n")
-            newlines.append("#" + line)
-            newlines.append(linetoappend + "\n")
-            done = True
-        elif line.startswith(linetoappend):
-            alreadydone = True
-            print "already done"
-            break
-        else:
-            newlines.append(line)
-
-    if alreadydone:
+    if linetocomment in text:
+        newtext = text.replace(linetocomment,linetoappend)
+    elif linetoappend in text:
+        print "setup_autologin: file seems to be already done"
         return None
+    else:
+        raise BaseException('could not find linetocomment or linetoappend in file')
 
-    if not done:
-        raise BaseException('the string was not found in the file')
-
-    os.remove(filepath)
-    f = file(filepath, "w")
-    f.write("".join(newlines))
+    f = file(filepath, "w+")
+    f.write(newtext)
     f.close()
-    print "setup_autologin completed"
+
+    print "setup_autologin: completed"
 
 def setup_autostart():
+    #apends a line to a file
     filepath = "/home/pi/.bashrc"
-    #filepath = r"C:\Users\omarg_000\Desktop\test.txt"
     linetoappend = r". /home/pi/pylog485/start.sh"
 
     f = file(filepath,"r")
     s = f.readlines()
     f.close()
-    if linetoappend in "".join(s):
-        print "line already there"
+    if any([ss.strip() == linetoappend for ss in s]):
+        print "setup_autostart: already done"
         return None
-    else:
-        f = file(filepath, "w")
-        f.writelines(s + [linetoappend])
-        f.close()
 
-    print "setup_autologin successful"
-    return None
+    f = file(filepath,"w+")
+    s = f.writelines(s + [linetoappend])
+    f.close()
+    print "setup_autostart: successful"
 
 def setup_internetsettings():
-    f = file("/etc/wpa_supplicant/wpa_supplicant.conf", "w")
+    #write file1
+    f = file("/etc/wpa_supplicant/wpa_supplicant.conf", "w+")
     contents = """
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
@@ -108,17 +84,14 @@ network={{
 }}""".format(js)
     f.write(contents)
     f.close()
+    print "setup_internetsettings: wpa_supplicant.conf successful"
 
+    #write file2
     f = file("sudo nano /etc/network/interfaces", "r+")
     contents="""
 auto lo
 iface lo inet loopback
 iface eth0 inet dhcp
-
-#### for a wlan DHCP ip
-#iface wlan0 inet dhcp
-#   wpa-ssid "xx"
-#   wpa-psk "xx"
 
 #### for a wlan static ip
 allow-hotplug wlan0
@@ -136,28 +109,34 @@ iface default inet static
     gateway {0[network][gateway]}
 
 """.format(js)
-    print "setup_staticwifi: successful"
+    print "setup_internetsettings: interfaces successful"
 
-def setup_realtimeclock():
-    execute("wget http://afterthoughtsoftware.com/files/linux-image-3.6.11-atsw-rtc_1.0_armhf.deb&&sudo dpkg -i linux-image-3.6.11-atsw-rtc_1.0_armhf.deb&&sudo cp /boot/vmlinuz-3.6.11-atsw-rtc+ /boot/kernel.img")
-    filepath = "/etc/modules"
-    toappend = [
-        "i2c-bcm2708"
-        ,"rtc-pcf2127a"
-        ]
+def setup_realtimeclock(typ):
+    print "not ready for this yet"
+    return None
+    if typ == "rasclock":
+        execute("wget http://afterthoughtsoftware.com/files/linux-image-3.6.11-atsw-rtc_1.0_armhf.deb&&sudo dpkg -i linux-image-3.6.11-atsw-rtc_1.0_armhf.deb&&sudo cp /boot/vmlinuz-3.6.11-atsw-rtc+ /boot/kernel.img")
 
-    filepath = "/etc/rc.local"
+        filepath = "/etc/modules"
+        toappend = [
+            "i2c-bcm2708"
+            ,"rtc-pcf2127a"
+            ]
 
-    toappend = [ #just before exit 0
-         "echo pcf2127a 0x51 > /sys/class/i2c-adapter/i2c-1/new_device"
-        ,"( sleep 2; hwclock -s ) &"
-         ]
+        filepath = "/etc/rc.local"
+
+        toappend = [ #just before exit 0
+             "echo pcf2127a 0x51 > /sys/class/i2c-adapter/i2c-1/new_device"
+            ,"( sleep 2; hwclock -s ) &"
+             ]
+    else:
+        raise BaseException('we do not know how to install this clock %s' %typ)
 
 
-if False and __name__ == "__main__":
+if __name__ == "__main__":
     execute([
-         'sudo apt-get install python-dev <<<y'
-        ,'sudo apt-get install python-pip <<<y'
+         'sudo apt-get install -y python-dev'
+        ,'sudo apt-get install -y python-pip'
         ,'sudo apt-get install tmux'
         ,'sudo pip install django==1.7'
         ,'sudo pip install pymodbus==1.2.0'
@@ -167,15 +146,5 @@ if False and __name__ == "__main__":
     setup_autologin()
     setup_autostart()
     setup_internetsettings()
-    if js["realtimeclock"]["installed"]:
-        setup_realtimeclock()
-
-
-if False:
-    fp = r"C:\Users\omarg_000\Desktop\test.txt"
-    f = file(fp, "r")
-    s=f.readlines()
-    f.close()
-    f = file(fp, "w")
-    f.writelines(s)
-    f.close()
+    if "rtc" in js:
+        setup_realtimeclock(js['type'])

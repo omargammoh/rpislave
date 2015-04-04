01c05c3a152c3e8a4de2c22feb4676de9d862f3a
Omar Gammoh <omar.gammoh@gmail.com>
2015-04-04 17:33:10 +0200

    nodaemon

--- btsync_app/process.py
+++ btsync_app/process.py
@@ -1,9 +1,8 @@
 import subprocess
-from time import sleep
 import traceback
 import os
 
-cmd = "./btsync --webui.listen 0.0.0.0:9004"
+cmd = "./btsync --nodaemon --webui.listen 0.0.0.0:9004"
 
 def main():
     pid = None
@@ -11,23 +10,27 @@ def main():
         #make sure the data folder is there with the correct permissions
         if not os.path.isdir("/home/pi/data"):
             os.mkdir("/home/pi/data")
-        _ = subprocess.Popen('sudo chmod -R 0777 "/home/pi/data"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+        pc = subprocess.Popen('sudo chmod -R 0777 "/home/pi/data"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        pc.communicate()
 
         #run server
         pc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=r'/home/pi')
         pid = pc.pid
+        pc.communicate()
 
         #stay here if everything is ok and no dignal is given
-        _ = pc.communicate()
 
     except KeyboardInterrupt:
         if pid:
-            s = subprocess.Popen("sudo kill -INT %s" % pid, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
-            print 'btsync_app interrupted successfuly'
-        else: print 'btsync_app: no pid'
+            pc = subprocess.Popen("sudo kill -INT %s" % pid, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            pc.communicate()
+            print 'btsync_app: interrupted successfuly'
+        else:
+            print 'btsync_app: no pid'
 
     except:
         print traceback.format_exc()
         raise
+
 
 

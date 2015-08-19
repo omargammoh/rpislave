ef88b1b722df1cf52e6740f0048f5af471c63fd0
Omar Gammoh <omar.gammoh@gmail.com>
2015-08-19 21:01:19 +0200

    change base_url ip to test it

--- website/settings.py
+++ website/settings.py
@@ -151,4 +151,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     )
 
 LOGIN_EXEMPT_URLS = ("^motion_app/register_event/$", "^datalog_app/highresmcp3008/$")
-BASE_URL = "http://rpi-master.com/api/slave/?"
+#BASE_URL = "http://rpi-master.com/api/slave/?"
+BASE_URL = "http://52.24.252.161/api/slave/?"
+

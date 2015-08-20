30ae55c64debaa6da76dfef6300ba12ed432a45f
Omar Gammoh <omar.gammoh@gmail.com>
2015-08-20 21:52:49 +0200

    minor

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ sudo git pull
 sudo python manage.py migrate --noinput
 tmux kill-session -t rpislave
 sudo chmod 700 /home/pi/rpislave/tunnelonly
+echo 'restarting in 3 seconds'
+sleep 3
 sudo reboot

2780d5746fcd7df83db60e1a40c0616c2fba3855
Omar Gammoh <omar.gammoh@gmail.com>
2015-09-13 22:02:52 +0200

    fix

--- setup.py
+++ setup.py
@@ -251,7 +251,7 @@ if __name__ == "__main__":
     t1 = time()
     _execute([
          "sudo apt-get -y update", #update is needed for motion
-         "sudo apt-get autossh"
+         "sudo apt-get install autossh"
     ])
     t2 = time()
     _execute([

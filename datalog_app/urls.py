ae6f45149cfe369a456ee8bc9a3f3275575a7ae9
Omar Gammoh <omar.gammoh@gmail.com>
2015-11-20 11:29:23 +0100

    small fix

--- datalog_app/urls.py
+++ datalog_app/urls.py
@@ -4,7 +4,6 @@ from datalog_app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home),
-    url(r'^recentdata/$', views.recentdata),
     url(r'^highresmcp3008/$', views.highresmcp3008),
     url(r'^highchart/$', views.highchart),
     url(r'^_getdata/$', views._getdata),
